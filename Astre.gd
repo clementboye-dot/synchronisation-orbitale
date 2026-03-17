2e3c3674c5d07355846881baed096e838ddedb1a
Marie Guillemette <marieg@MacBook-Pro-de-Marie.local>
2026-03-17 14:01:47 -0400

    allo

--- Astre.gd
+++ Astre.gd
@@ -1,4 +1,9 @@
+#Simule la révolution des lunes autour de Jupiter
 extends RigidBody3D
+class_name Astre
+
+@export var centre_rotation : RigidBody3D
+@export var periode_relative : float
 
 
 # Called when the node enters the scene tree for the first time.
