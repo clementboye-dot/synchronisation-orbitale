ddfd7bb9cbe0f9c16d5360ff7311d96bf825158e
Clément Boyé <clement_boye@icloud.com>
2026-03-20 15:22:17 -0400

    Essayer de faire fonctionner

--- Astre.gd
+++ Astre.gd
@@ -1,8 +1,8 @@
 #Simule la révolution des lunes autour de Jupiter
 extends RigidBody3D
-class_name Astre
+#class_name Astre
 
-@export var centre_rotation : RigidBody3D
+@export var jupiter : RigidBody3D
 @export var periode_relative : float
 
 @export_group("Paramètre de conversion simulation")
@@ -13,38 +13,47 @@ class_name Astre
 
 @export_group("Simulation gravitationnelle")
 @export var masse : float
-@export var masse_corps_rotation : float
-@export var rayon_initial : float
-@export var vitesse_initiale : float
+@export var masse_jupiter : float
+
 
 @export_group("Paramètres de la méthode d'Euler")
 @export var etapes_calcul_par_ecran : int
 
-# Groupe de variable masquées pour les calculs
+#---CONSTANTES---#
 var G : float = 6.673e-11
-var R_p : float = 664862000 #m
+var R_p : float = 664862000 #((m)
+var V_p : float = 14.193e3 #(m/s)
 var K : float = 1e14 #(N/m)
 var D : float = 24.97e6 #(m)
 var Zeta : float = 4e16 #(N⋅s/m)
 
 
+var r_i1 : Vector3 
+var r_i2 : Vector3
+var v_i1 : Vector3
+var v_i2 : Vector3
 
-var r_i : Vector3
-var v_i : Vector3
 var periode : float
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	r_i = rayon_initial * Vector3(1, 0, 0)
-	position = conv_position_reelle_a_simulee(r_i)
+	r_i1 = (R_p - D/2) * Vector3(1, 0, 0)
+	r_i2 = (R_p + D/2) * Vector3(1, 0, 0)
+	position = conv_position_reelle_a_simulee(r_i1)
 	
-	var v_initiale = vitesse_initiale * Vector3(0, 0, 1)
+	v_i1 = (3/4) * V_p * Vector3(0, 0, 1)
+	v_i2 = (5/4) * V_p * Vector3(0, 0, 1)
+	
+	periode = 2 * PI * r_i1.length() / v_i1.length()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var fg1 : Vector3 = force_gravitationnelle(r_i1)
+	var fg2 : Vector3 = force_gravitationnelle(r_i2)
+	
 	
 
 
@@ -57,7 +66,7 @@ func force_gravitationnelle(position_reelle : Vector3) -> Vector3:
 	Retour:
 		La force gravitationnelle de Jupiter sur le point de la lune Europe.
 	"""
-	var force_g = -1 * G * masse * masse_corps_rotation * position_reelle / (position_reelle.length()**3)
+	var force_g = -1 * G * masse * masse_jupiter * position_reelle / (position_reelle.length()**3)
 	return force_g
 
 
@@ -91,8 +100,8 @@ func calculer_acceleration_gravitationnelle(position_reelle: Vector3) -> Vector3
 	Retour:
 	L'accélération gravitationnelle exercée sur le corps en m/s^2
 	"""
-	var facteur = -G * masse * masse_corps_rotation / (position_reelle.length()**3)
-	var force = (position_reelle - centre_rotation.position) * facteur
+	var facteur = -G * masse * masse_jupiter / (position_reelle.length()**3)
+	var force = (position_reelle - jupiter.position) * facteur
 	return force / masse
 
 func appliquer_euler(temps_dernier_ecran : float) -> void:
@@ -109,10 +118,10 @@ func appliquer_euler(temps_dernier_ecran : float) -> void:
 	var h = nb_periode / etapes_calcul_par_ecran
 		
 	for i in range(etapes_calcul_par_ecran):
-		var a_i = calculer_acceleration_gravitationnelle(r_i)
+		var a_i = calculer_acceleration_gravitationnelle(r_i1)
 		
-		var r_i_plus_1 = r_i + h * v_i
-		var v_i_plus_1 = v_i + h * a_i
+		var r_i_plus_1 = r_i1 + h * v_i1
+		var v_i_plus_1 = v_i1 + h * a_i
 		
-		r_i = r_i_plus_1
-		v_i = v_i_plus_1
+		r_i1 = r_i_plus_1
+		v_i1 = v_i_plus_1
